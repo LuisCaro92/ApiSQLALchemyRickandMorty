ff0bd7fe19b19410197c91a65f91e129b7f9ba23
Luis Caro <luis.carosalgado1@gmail.com>
2023-03-10 21:32:40 -0300

    logrado

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask,request, jsonify
-from models import db, User, Character
+from models import db, User, Character, Location, Favorito
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
@@ -21,6 +21,16 @@ def create_user():
 
     return "Usuario guardado"
 
+@app.route("/character/<int:id>", methods=["GET"])
+def get_oneuser(id):
+    user = User.query.get(id)
+    if user is not None:
+       return jsonify(user.serialize())
+
+    else:
+        return jsonify("No encontrado"), 418   
+    
+    
 @app.route("/users/list", methods=["GET"])
 def get_users():
     users = User.query.all()
@@ -40,7 +50,8 @@ def update_user(id):
             return jsonify("Eliminado"), 204
         else:
             user.age = request.json.get("age")
-            
+            user.name =request.json.get("name")
+            user.password = request.json.get("password")
             db.session.commit()
             
             return jsonify("Usuario actualizado"), 200
@@ -48,7 +59,9 @@ def update_user(id):
     return jsonify("Usuario no encontrado"), 418
 
 
-@app.route("/add/people", methods=["POST"])
+## personajes
+
+@app.route("/add/character", methods=["POST"])
 def add_people():
     character = Character()
     character.name = request.json.get("name")
@@ -72,6 +85,15 @@ def get_characters():
         result.append(character.serialize())
     return jsonify(result)
 
+@app.route("/character/<int:id>", methods=["GET"])
+def get_onecharacter(id):
+    character = Character.query.get(id)
+    if character is not None:
+       return jsonify(character.serialize())
+
+    else:
+        return jsonify("No encontrado"), 418   
+
 
 
 @app.route("/character/<int:id>", methods=["PUT", "DELETE"])
@@ -84,14 +106,135 @@ def update_character(id):
 
             return jsonify("Eliminado"), 204
         else:
-            character.age = request.json.get("age")
+            character.name = request.json.get("name")
+            character.status = request.json.get("status")
+            character.species = request.json.get("species")
+            character.gender = request.json.get("gender")
             
             db.session.commit()
             
-            return jsonify("Usuario actualizado"), 200
+            return jsonify("Personaje actualizado"), 200
     
-    return jsonify("Usuario no encontrado"), 418
+    return jsonify("Personaje no encontrado"), 418
 
+
+
+
+ ## Location
+
+
+
+@app.route("/add/location", methods=["POST"])
+def add_location():
+    location = Location()
+    location.name = request.json.get("name")
+    location.dimension = request.json.get("dimension")
+    location.residents = request.json.get("residents")
+  
+   
+    db.session.add(location)
+    db.session.commit()
+
+    return "Locacion guardado"
+
+
+@app.route("/location/<int:id>", methods=["GET"])
+def get_onelocation(id):
+    location = Location.query.get(id)
+    if location is not None:
+       return jsonify(location.serialize())
+
+    else:
+        return jsonify("No encontrado"), 418  
+    
+
+@app.route("/location/list", methods=["GET"])  
+def get_locations():
+    locations = Location.query.all()
+    result = []
+    for location in locations:
+        result.append(location.serialize())
+    return jsonify(result)
+
+
+
+@app.route("/location/<int:id>", methods=["PUT", "DELETE"])
+def update_location(id):
+    location = Location.query.get(id)
+    if location is not None:
+        if request.method == "DELETE":
+            db.session.delete(location)
+            db.session.commit()
+
+            return jsonify("Eliminado"), 204
+        else:
+            location.name = request.json.get("name"),
+            location.dimension = request.json.get("dimension"),
+            location.residents = request.json.get("residents")
+            
+            db.session.commit()
+            
+            return jsonify("locacion actualizado"), 200
+    
+    return jsonify("Locacion no encontrado"), 418   
+
+##  Favoritos
+
+
+@app.route("/add/favorite", methods=["POST"])
+def add_favorite():
+    favorito = Favorito()
+    favorito.user = request.json.get("user")
+    favorito.character = request.json.get("character")
+    favorito.location = request.json.get("location")
+  
+   
+    db.session.add(favorito)
+    db.session.commit()
+
+    return "Favorito guardado"
+
+@app.route("/favorito/<int:id>", methods=["GET"])
+def get_onefavorito(id):
+    favorito = Favorito.query.get(id)
+    if favorito is not None:
+       return jsonify(favorito.serialize())
+
+    else:
+        return jsonify("No encontrado"), 418  
+    
+
+@app.route("/favorito/list", methods=["GET"])  
+def get_favoritos():
+    favoritos = favorito.query.all()
+    result = []
+    for favorito in favoritos:
+        result.append(favorito.serialize())
+    return jsonify(result)
+
+
+
+@app.route("/favorito/<int:id>", methods=["PUT", "DELETE"])
+def update_favorito(id):
+    favorito = Favorito.query.get(id)
+    if favorito is not None:
+        if request.method == "DELETE":
+            db.session.delete(favorito)
+            db.session.commit()
+
+            return jsonify("Eliminado"), 204
+        else:
+            
+            favorito.user = request.json.get("user"),
+            favorito.character = request.json.get("character"),
+            favorito.location = request.json.get("location")
+  
+            
+            db.session.commit()
+            
+            return jsonify("Favorito actualizado"), 200
+    
+    return jsonify("Favorito no encontrado"), 418
 
 with app.app_context():
     db.create_all()
--- models.py
+++ models.py
@@ -8,6 +8,7 @@ class User(db.Model):
     username = db.Column(db.String(50), nullable=False)
     password = db.Column(db.String(20), nullable=False)
     age = db.Column(db.Integer)
+    favorito = db.relationship("Favorito")
 
     def serialize(self):
         return {
@@ -23,7 +24,7 @@ class Character(db.Model):
     status = db.Column(db.String(200), nullable=False)
     species = db.Column(db.String(200), nullable=False)
     gender = db.Column(db.String(200), nullable=False)
-   
+    favorito = db.relationship("Favorito")
     
 
     def serialize(self):
@@ -36,26 +37,34 @@ class Character(db.Model):
         }
 
 
-#class Location(db.Model):
- #   __tablename__ = 'location'
-  #  id = db.Column(db.Integer, primary_key=True)
-   # name = db.Column(db.String(200), nullable=False)
-    #dimension = db.Column(db.String(200), nullable=False)
-    #residents = db.Column(db.String(200), db.ForeignKey("character.name"))
+class Location(db.Model):
+    __tablename__ = 'location'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(200), nullable=False)
+    dimension = db.Column(db.String(200), nullable=False)
+    favorito = db.relationship("Favorito")
  
+    
+    def serialize(self):
+        return{
+            "name": self.name,
+            "dimension": self.dimension,
+            "residents": self.residents
+        }
+        
 
-#class Episode(db.Model):
-   # __tablename__ = 'episode'
-    #id = db.Column(db.Integer, primary_key=True)
-    #name = db.Column(db.String(200), nullable=False)
-    #air_date = db.Column(db.String(200), nullable=False)
-    #episode = db.Column(db.String(200), nullable=False)
-    #character=db.Column(db.String(200),db.ForeignKey("character.name"))
 
-#class Favorito(db.Model):
- #   __tablename__ = 'favorito'
-  #  id = db.Column(db.Integer, primary_key=True)
-   # personaje = db.Column(db.String(200),db.ForeignKey("character.name"))
-    #user = db.Column(db.String(200),db.ForeignKey("user.name"))
-
+class Favorito(db.Model):
+    __tablename__ = 'favorito'
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.String(30),db.ForeignKey("user.username"))
+    character = db.Column(db.String(30), db.ForeignKey("character.name"))
+    location = db.Column(db.String(30), db.ForeignKey("location.name"))
+    
+    def serialize(self):
+        return{
+            "user": self.user,
+            "character": self.character,
+            "location": self.location
+        }
 
